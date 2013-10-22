675c85111b9ad2ece2329126d33d0086572b1c74
Jordan Dukart <jordan@discoverygarden.ca>
2013-10-22 17:45:41 +0000

    Fix resumption token bug, other bugs, coding standards.

---
+++ islandora_oai.api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by Islandora OAI.
+ */
+
+/**
+ * Retrieve module supplied XSL files for transforming content to output for OAI.
+ *
+ * @return array
+ *   Returns an associative array where the key is the URI to the file on the
+ *   file system and the value is the name of the file.
+ */
+function hook_islandora_oai_get_xsl_files() {
+}
